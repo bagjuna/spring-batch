db244d71bfc0750e2baeb1b3054df7c7422c9859
bagjuna <juna.park124@gmail.com>
2025-12-05 12:42:16 +0900

    Job Instance

--- src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
@@ -2,11 +2,8 @@ package io.springbatch.springbatchlecture;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -28,33 +25,22 @@ public class JobConfiguration {
 			.build();
 	}
 
-	@Bean
-	public Step step1() {
+	private Step step1() {
 		return stepBuilderFactory.get("step1")
-			.tasklet(new Tasklet() {
-				@Override
-				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-					System.out.println("step1 has executed");
-					return RepeatStatus.FINISHED;
-				}
+			.tasklet((contribution, chunkContext) -> {
+				return RepeatStatus.FINISHED;
 			})
 			.build();
+
 	}
 
-
-	@Bean
-	public Step step2() {
+	private Step step2() {
 		return stepBuilderFactory.get("step2")
-			.tasklet(new Tasklet() {
-				@Override
-				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-					System.out.println("step2 has executed");
-					return RepeatStatus.FINISHED;
-				}
+			.tasklet((contribution, chunkContext) -> {
+				return RepeatStatus.FINISHED;
 			})
 			.build();
+
 	}
-
-
 
 }
