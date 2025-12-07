6653adc65ab1ff51ee8fed4048991300ce494494
bagjuna <juna.park124@gmail.com>
2025-12-07 14:46:13 +0900

    job and step

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -10,17 +10,15 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Configuration
-public class JobLauncherConfiguration {
-
-
+@RequiredArgsConstructor
+public class JobConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job BatchJob() {
-		return jobBuilderFactory.get("Job")
+	public Job job2() {
+		return this.jobBuilderFactory.get("batchJob1")
 			.start(step1())
 			.next(step2())
 			.build();
@@ -28,20 +26,19 @@ public class JobLauncherConfiguration {
 
 	@Bean
 	public Step step1() {
-		return stepBuilderFactory.get("step1")
+		return this.stepBuilderFactory.get("step1")
 			.tasklet((contribution, chunkContext) -> {
-				Thread.sleep(3000);
+				System.out.println("step1 has executed");
 				return RepeatStatus.FINISHED;
 			})
 			.build();
 	}
 
-
-
 	@Bean
 	public Step step2() {
-		return stepBuilderFactory.get("step2")
+		return this.stepBuilderFactory.get("step2")
 			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step2 has executed");
 				return RepeatStatus.FINISHED;
 			})
 			.build();
