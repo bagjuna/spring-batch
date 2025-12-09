550c043d381858d6c542e513f02f728a8fe25610
bagjuna <juna.park124@gmail.com>
2025-12-09 20:31:20 +0900

    10 스프링 배치 실행 job (#12)
    * jobBuilderFactory
    # Conflicts:
    #	src/main/java/io/springbatch/springbatchlecture/ValidatorConfiguration.java

--- src/main/java/io/springbatch/springbatchlecture/ValidatorConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ValidatorConfiguration.java
@@ -1,7 +1,5 @@
 package io.springbatch.springbatchlecture;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
