98a493b27542e7b26daf2545d6a8258bc1f8e154
bagjuna <juna.park124@gmail.com>
2025-12-10 23:29:41 +0900

    PreventRestart

--- src/main/java/io/springbatch/springbatchlecture/PreventRestartConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/PreventRestartConfiguration.java
@@ -15,7 +15,8 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class PreventRestartConfiguration {
+
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
@@ -24,9 +25,7 @@ public class ValidatorConfiguration {
 		return this.jobBuilderFactory.get("batchJob")
 			.start(step1())
 			.next(step2())
-			.next(step3())
-			// .validator(new CustomJobParametersValidator())
-			.validator(new DefaultJobParametersValidator(new String[] {"name", "date"}, new String[] {"count"}))
+			.preventRestart()
 			.build();
 	}
 
@@ -44,42 +43,14 @@ public class ValidatorConfiguration {
 	public Step step2() {
 		return this.stepBuilderFactory.get("step2")
 			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step2 has executed");
-				return RepeatStatus.FINISHED;
+				throw new RuntimeException("step2 has failed");
+				// System.out.println("step2 has executed");
+				// return RepeatStatus.FINISHED;
 			})
 			.build();
 	}
 
-	@Bean
-	public Step step3() {
-		return this.stepBuilderFactory.get("step3")
-			.tasklet((contribution, chunkContext) -> {
-				chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-				contribution.setExitStatus(ExitStatus.STOPPED);
-				System.out.println("step3 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
-	}
 
-	@Bean
-	public Step step4() {
-		return this.stepBuilderFactory.get("step4")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step4 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
-	}
 
-	@Bean
-	public Step step5() {
-		return this.stepBuilderFactory.get("step5")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step5 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
 
-	}
 }
