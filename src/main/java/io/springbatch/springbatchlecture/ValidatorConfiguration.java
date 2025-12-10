7642123e86f926ce1539bbaef40f0fe8bf8a2298
bagjuna <juna.park124@gmail.com>
2025-12-10 23:02:39 +0900

    validator

--- src/main/java/io/springbatch/springbatchlecture/ValidatorConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ValidatorConfiguration.java
@@ -1,12 +1,12 @@
 package io.springbatch.springbatchlecture;
 
-
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,19 +15,21 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class ValidatorConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job batchJob2() {
-		return this.jobBuilderFactory.get("batchJob2")
-			.start(flow())
-			.next(step5())
-			.end()
+	public Job batchJob1() {
+		return this.jobBuilderFactory.get("batchJob")
+			.start(step1())
+			.next(step2())
+			.next(step3())
+			// .validator(new CustomJobParametersValidator())
+			.validator(new DefaultJobParametersValidator(new String[] {"name", "date"}, new String[] {"count"}))
 			.build();
 	}
-/*
+
 	@Bean
 	public Step step1() {
 		return this.stepBuilderFactory.get("step1")
@@ -49,52 +51,15 @@ public class JobConfiguration {
 	}
 
 	@Bean
-	public Job batchJob1() {
-		return this.jobBuilderFactory.get("batchJob1")
-			.start(step1())
-			.next(step2())
-			.build();
-	}
-	*/
-
-	@Bean
-	public Flow flow() {
-		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-		flowBuilder.start(step3())
-			.next(step4())
-			.end();
-
-		return flowBuilder.build();
-	}
-
-	@Bean
 	public Step step3() {
 		return this.stepBuilderFactory.get("step3")
 			.tasklet((contribution, chunkContext) -> {
+				chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+				contribution.setExitStatus(ExitStatus.STOPPED);
 				System.out.println("step3 has executed");
 				return RepeatStatus.FINISHED;
 			})
 			.build();
 	}
 
-	@Bean
-	public Step step4() {
-		return this.stepBuilderFactory.get("step4")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step4 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
-	}
-
-	@Bean
-	public Step step5() {
-		return this.stepBuilderFactory.get("step5")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step5 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
-
-	}
 }
