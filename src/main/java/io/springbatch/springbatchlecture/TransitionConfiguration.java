a09975bc367220ed58c09b9530c3c26507d12c45
bagjuna <juna.park124@gmail.com>
2025-12-20 22:31:54 +0900

    Transition - on() / to() / stop(), fail(), end(), stopAndRestart()

--- src/main/java/io/springbatch/springbatchlecture/TransitionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/TransitionConfiguration.java
@@ -1,70 +1,50 @@
 package io.springbatch.springbatchlecture;
 
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 
-@Configuration
 @RequiredArgsConstructor
-public class StartNextConfiguration {
-
+@Configuration
+public class TransitionConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
 	public Job batchJob() {
 		return this.jobBuilderFactory.get("batchJob")
-			.start(flowA())
-			.next(step3())
-			.next(flowB())
-			.next(step6())
+			.start(step1())
+				.on("FAILED")  // step1의 ExitStatus가 FAILED일 때
+				.to(step2())   // step2로 이동
+				.on("FAILED") // step2의 ExitStatus가 FAILED일 때
+				.stop()
+			.from(step1())
+				.on("*")
+				.to(step3())
+				.next(step4())
+			.from(step2())
+				.on("*")
+				.to(step5())
 			.end()
 			.build();
-	}
-
-	@Bean
-	public Flow flowA() {
-
-		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
-
-		flowBuilder.start(step1())
-			.next(step2())
-			.end();
-
-		return flowBuilder.build();
-
-	}
-
-	@Bean
-	public Flow flowB() {
-
-		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
-		flowBuilder.start(step4())
-			.next(step5())
-			.end();
-
-		return flowBuilder.build();
 	}
 
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
 			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step1 has executed");
-				return RepeatStatus.FINISHED;
+				// System.out.println("step1 has executed");
+				throw new RuntimeException("step1 has failed");
+				// return RepeatStatus.FINISHED;
 			})
 			.build();
 	}
-
 
 	@Bean
 	public Step step2() {
@@ -86,6 +66,7 @@ public class StartNextConfiguration {
 			.build();
 	}
 
+
 	@Bean
 	public Step step4() {
 		return stepBuilderFactory.get("step4")
@@ -96,22 +77,11 @@ public class StartNextConfiguration {
 			.build();
 	}
 
-
 	@Bean
 	public Step step5() {
 		return stepBuilderFactory.get("step5")
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step5 has executed");
-				return RepeatStatus.FINISHED;
-			})
-			.build();
-	}
-
-	@Bean
-	public Step step6() {
-		return stepBuilderFactory.get("step6")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step6 has executed");
 				return RepeatStatus.FINISHED;
 			})
 			.build();
