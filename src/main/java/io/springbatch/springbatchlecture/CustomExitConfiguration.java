bf3f76e2f22cb06c3a7f3ecef3f12bddde4a4706
bagjuna <juna.park124@gmail.com>
2025-12-22 22:43:54 +0900

    사용자 정의 ExitStatus
    * 사용자 정의 ExitStatus

---
+++ src/main/java/io/springbatch/springbatchlecture/CustomExitConfiguration.java
@@ -0,0 +1,56 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class CustomExitConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job batchJob() {
+		return this.jobBuilderFactory.get("batchJob")
+			.start(step1())
+			.on("FAILED")
+			.to(step2())
+			.on("PASS")
+			.stop()
+
+			.end()
+			.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step1 has executed");
+				// ExitStatus를 FAILED로 설정
+				contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+				return RepeatStatus.FINISHED;
+			})
+			.build();
+	}
+
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("step2")
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step2 has executed");
+				return RepeatStatus.FINISHED;
+			})
+			.build();
+	}
+
+}
