5a62ff7a786007450b655554c8409e2dbb7303c5
bagjuna <juna.park124@gmail.com>
2026-01-10 22:37:19 +0900

    ChunkOrientedTasklet

--- src/main/java/io/springbatch/springbatchlecture/ChunkOrientedTaskletConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ChunkOrientedTaskletConfiguration.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
@@ -18,49 +19,46 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-public class ChunkConfiguration {
+public class ChunkOrientedTaskletConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
 	public Job job() {
-		return this.jobBuilderFactory
-			.get("batchJob")
+		return jobBuilderFactory.get("batchJob")
 			.start(step1())
 			.next(step2())
 			.build();
 	}
 
 	@Bean
+	@JobScope
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
-			.<String, String>chunk(5)
-			.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+			.<String, String>chunk(3)
+			.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5", "item6")))
 			.processor(new ItemProcessor<String, String>() {
 				@Override
 				public String process(String item) throws Exception {
-					Thread.sleep(300);
-					System.out.println("item = " + item);
-					return "my" + item;
+					return "my_" + item;
 				}
 			})
 			.writer(new ItemWriter<String>() {
 				@Override
 				public void write(List<? extends String> items) throws Exception {
-					Thread.sleep(300);
-					System.out.println("items = " + items);
+					items.forEach(item -> System.out.println(item));
 				}
 			})
 			.build();
 	}
+
 	@Bean
 	public Step step2() {
 		return stepBuilderFactory.get("step2")
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step2 has executed");
 				return RepeatStatus.FINISHED;
-			})
-			.build();
+			}).build();
 	}
 }
