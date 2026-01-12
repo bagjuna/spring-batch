a646bc0219098e726e853686a9c7bd4d7b09066f
bagjuna <juna.park124@gmail.com>
2026-01-13 00:58:09 +0900

    FlatFilesReader - fixedlengthtokenizer

--- src/main/java/io/springbatch/springbatchlecture/FlatFilesFixedConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/FlatFilesFixedConfiguration.java
@@ -6,21 +6,21 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-public class FlatFilesConfiguration {
-
+public class FlatFilesFixedConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
@@ -42,13 +42,28 @@ public class FlatFilesConfiguration {
 			.writer(new ItemWriter() {
 				@Override
 				public void write(List items) throws Exception {
-					System.out.println("items = " + items);
+					items.forEach(item -> System.out.println(item));
 				}
 			})
 			.build();
 	}
 
 
+	public FlatFileItemReader itemReader() {
+		return new FlatFileItemReaderBuilder<Customer>()
+			.name("flatFile")
+			.resource(new FileSystemResource(
+				"absolute-path"))
+			.fieldSetMapper(new BeanWrapperFieldSetMapper<>())
+			.targetType(Customer.class)
+			.linesToSkip(1)
+			.fixedLength()
+			.addColumns(new Range(1, 5))
+			.addColumns(new Range(6, 9))
+			.addColumns(new Range(10, 11))
+			.names("name", "year", "age")
+			.build();
+	}
 
 	@Bean
 	public Step step2() {
@@ -59,20 +74,5 @@ public class FlatFilesConfiguration {
 			})
 			.build();
 	}
-
-	@Bean
-	public ItemReader itemReader() {
-		return new FlatFileItemReaderBuilder<Customer>()
-			.name("flatFile")
-			.resource(new ClassPathResource("/customer.csv"))
-			.fieldSetMapper(new CustomerFieldSetMapper())
-			.targetType(Customer.class)
-			.linesToSkip(1)
-			.delimited().delimiter(",")
-			.names("name", "age", "year")
-			.build();
-
-	}
-
 
 }
