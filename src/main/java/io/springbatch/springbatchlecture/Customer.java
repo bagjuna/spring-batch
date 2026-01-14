8862cc685ddf6c1b88de0537854692191772fa63
bagjuna <juna.park124@gmail.com>
2026-01-14 22:20:41 +0900

    FlatFilesReader - JpaCursorItemReader

--- src/main/java/io/springbatch/springbatchlecture/Customer.java
+++ src/main/java/io/springbatch/springbatchlecture/Customer.java
@@ -1,15 +1,21 @@
 package io.springbatch.springbatchlecture;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+@Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Customer {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String firstName;
 	private String lastName;
