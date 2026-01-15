bb389b9b1386115cb89c0d5a522a04d79440f3a2
bagjuna <juna.park124@gmail.com>
2026-01-16 00:00:56 +0900

    XML StaxEventItemWriter

---
+++ src/main/resources/schema-mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `customer` (
+                            `id` mediumint(8) unsigned NOT NULL auto_increment,
+                            `first_name` varchar(255) default NULL,  -- 여기 수정
+                            `last_name` varchar(255) default NULL,   -- 여기 수정
+                            `birthdate` varchar(255),
+                            PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
