90ecbb9471fccafdcbb91df3833cb486f683f5af
bagjuna <juna.park124@gmail.com>
2026-01-15 23:07:16 +0900

    FlatFileItemWriter - Delimited

--- src/main/java/io/springbatch/springbatchlecture/FlatFilesDelimitedConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/FlatFilesDelimitedConfiguration.java
@@ -1,6 +1,7 @@
 package io.springbatch.springbatchlecture;
 
-import javax.persistence.EntityManagerFactory;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -9,18 +10,19 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.adapter.ItemReaderAdapter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Configuration
-public class ItemReaderAdapterConfiguration {
+@RequiredArgsConstructor
+public class FlatFilesDelimitedConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
-	private final EntityManagerFactory entityManagerFactory;
 
 	@Bean
 	public Job job() throws Exception {
@@ -31,36 +33,35 @@ public class ItemReaderAdapterConfiguration {
 	}
 
 
-
-	@Bean
 	public Step step1() throws Exception {
 		return stepBuilderFactory.get("step1")
-			.<String, String>chunk(10)
+			.<Customer, Customer>chunk(10)
 			.reader(customItemReader())
 			.writer(customItemWriter())
 			.build();
 	}
 
 	@Bean
-	public ItemReader<String> customItemReader() {
+	public ItemWriter<? super Customer> customItemWriter() throws Exception {
+		return new FlatFileItemWriterBuilder<Customer>()
+			.name("flatFileItemWriter")
+			.resource(new FileSystemResource("/absolute/path/src/main/resources/customers.txt"))
+			.append(true)
+			.shouldDeleteIfEmpty(true)
+			.delimited()
+			.delimiter("|")
+			.names(new String[] { "id", "name", "age" })
+			.build();
+	}
 
-		ItemReaderAdapter<String> reader = new ItemReaderAdapter<>();
-		reader.setTargetObject(customService());
-		reader.setTargetMethod("customRead");
-
+	@Bean
+	public ItemReader<? extends Customer> customItemReader() {
+		List<Customer> customers = Arrays.asList(new Customer(1L, "hong gil dong1", 41),
+			new Customer(2L, "hong gil dong2", 42),
+			new Customer(3L, "hong gil dong3", 43)
+		);
+		ListItemReader<Customer> reader = new ListItemReader<>(customers);
 		return reader;
-
 	}
 
-	@Bean
-	public Object customService() {
-		return new CustomService();
-	}
-
-	@Bean
-	public ItemWriter<String> customItemWriter() {
-		return items -> {
-			System.out.println(items);
-		};
-	}
 }
