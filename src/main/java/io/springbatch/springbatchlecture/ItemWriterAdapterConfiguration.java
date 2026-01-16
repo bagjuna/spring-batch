7a73ba689c8b5e3c5b2ab4395425f16e836566b3
bagjuna <juna.park124@gmail.com>
2026-01-16 19:28:05 +0900

    ItemWriteAdapter

--- src/main/java/io/springbatch/springbatchlecture/ItemWriterAdapterConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ItemWriterAdapterConfiguration.java
@@ -3,7 +3,6 @@ package io.springbatch.springbatchlecture;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -13,7 +12,12 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.adapter.ItemWriterAdapter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -26,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class JpaConfiguration {
+public class ItemWriterAdapterConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -36,7 +40,7 @@ public class JpaConfiguration {
 	private String filePath;
 
 	@Bean
-	public Job job()  {
+	public Job job() {
 		return jobBuilderFactory.get("batchJob")
 			.incrementer(new RunIdIncrementer())
 			.start(step1())
@@ -46,53 +50,39 @@ public class JpaConfiguration {
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
-			.<Customer, Customer2>chunk(10)
-			.reader(customItemReader())
-			.processor(customItemProcessor())
+			.<String, String>chunk(10)
+			.reader(
+				new ItemReader<String>() {
+					int i = 0;
+
+					@Override
+					public String read() throws
+						Exception,
+						UnexpectedInputException,
+						ParseException,
+						NonTransientResourceException {
+						i++;
+						return i > 10 ? null : "item" + i;
+					}
+				}
+			)
 			.writer(customItemWriter())
 			.build();
 	}
 
 	@Bean
-	public ItemProcessor<? super Customer, ? extends Customer2> customItemProcessor() {
-		return new CustomItemProcessor();
+	public ItemWriter<? super String> customItemWriter() {
+
+		ItemWriterAdapter<String> writer = new ItemWriterAdapter<>();
+		writer.setTargetObject(customService());
+		writer.setTargetMethod("customWrite");
+
+		return writer;
 	}
 
 	@Bean
-	public ItemWriter<? super Customer2> customItemWriter() {
-		return new JdbcBatchItemWriterBuilder<Customer2>()
-			.dataSource(dataSource)
-			.sql("insert into customer2 (id, first_name, last_name, birthdate) values (:id, :firstName, :lastName, :birthdate)")
-			.beanMapped()
-			.build();
-	}
-
-	@Bean
-	public JdbcPagingItemReader<Customer> customItemReader() {
-
-		JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
-
-		reader.setDataSource(this.dataSource);
-		reader.setFetchSize(10);
-		reader.setRowMapper(new CustomerRowMapper());
-
-		MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
-		queryProvider.setSelectClause("id, first_name, last_name, birthdate");
-		queryProvider.setFromClause("from customer");
-		queryProvider.setWhereClause("where first_name like :firstname");
-
-		Map<String, Order> sortKeys = new HashMap<>(1);
-
-		sortKeys.put("id", Order.ASCENDING);
-		queryProvider.setSortKeys(sortKeys);
-		reader.setQueryProvider(queryProvider);
-
-		HashMap<String, Object> parameters = new HashMap<>();
-		parameters.put("firstname", "A%");
-
-		reader.setParameterValues(parameters);
-
-		return reader;
+	public CustomService customService() {
+		return new CustomService();
 	}
 
 }
