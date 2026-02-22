5e9e33241f0c8441b4faee3c7524078e10670839
bagjuna <juna.park124@gmail.com>
2026-02-22 16:33:31 +0900

    ChunkListener / ItemReadListener /ItemProcessListener /ItemWriteListener

---
+++ src/main/java/io/springbatch/springbatchlecture/CustomChunkListener.java
@@ -0,0 +1,27 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.AfterChunkError;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+//CustomChunkListener
+public class CustomChunkListener {
+
+	@BeforeChunk
+	public void beforeChunk(ChunkContext chunkContext) {
+		System.out.println(">> before Chunk");
+	}
+
+	@AfterChunk
+	public void afterChunk(ChunkContext chunkContext) {
+		System.out.println(">> after Chunk");
+	}
+
+	@AfterChunkError
+	public void afterChunkError(ChunkContext chunkContext) {
+		System.out.println(">> after Chunk error");
+	}
+
+
+}
